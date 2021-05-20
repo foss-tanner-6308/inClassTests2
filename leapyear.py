2efffba8784e7e793363e7105440d99db50ce6d8
Tanner <fossta@oregonstate.edu>
2021-05-20 16:33:34 -0700

    leap year function

--- leapyear.py
+++ leapyear.py
@@ -1,1 +1,14 @@
+def func(year):
 
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print(year, "is a leap year")
+                return 1
+            else:
+                print(year, "is not a leap year")
+        else:
+            print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+        return 2
