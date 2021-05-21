928ff02bae5e7ca55dc6f0bab35f5fa88a647252
Tanner <fossta@oregonstate.edu>
2021-05-20 17:09:49 -0700

    added tests

--- leapyear_tests.py
+++ leapyear_tests.py
@@ -1,4 +1,5 @@
-from leapyear import *
+import unittest
+from leap_year import *
 
 
 def test_leapyear_good():
@@ -7,3 +8,15 @@ def test_leapyear_good():
 
 def test_leapyear_bad():
     assert func(1999) == 2
+
+
+class TestCase(unittest.TestCase):
+    def test_pos(self):
+        self.assertEqual(func(2000), 1)
+
+    def test_neg(self):
+        self.assertEqual(func(1999), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
